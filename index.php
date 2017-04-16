e10358aeccfb30228092f962d3939c3b0c747d1e
Emirhan Pervanlar <pervanlaremirhan@gmail.com>
2017-04-16 18:15:30 +0300

    daha önceki çalışmamızı aktarıyoruz

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html lang="tr">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<link rel="stylesheet" href="style.css">	
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-design-iconic-font/2.2.0/css/material-design-iconic-font.min.css">
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+</head>
+
+<div id="topbar">
+<div class="container">	
+	<div class="topbarlogo"></div>
+		<div class="topbarmenu">
+			
+			<div class="topbarsearch">
+				<form>
+					<input type="text" name="search" placeholder="Search ..." />
+					<button type="submit" name="searchbutton" ><i class="zmdi zmdi-search"></i></button>
+				</form>
+			</div>
+			<div class="topbarsocial">
+				<li><i class="zmdi zmdi-facebook"></i></li>
+				<li><i class="zmdi zmdi-twitter"></i></li>
+				<li><i class="zmdi zmdi-dribbble"></i></li>
+				<li><i class="zmdi zmdi-lastfm"></i></li>
+				<li><i class="zmdi zmdi-linkedin"></i></li>
+				<li><i class="zmdi zmdi-tumblr"></i></li>
+				
+			</div>
+		</div>
+</div>
+		<div class="menu">
+		<div class="container">
+			<div class="menu-content">
+			<li>ENTAIREMENT</li>
+			<li>ENTAIREMENT</li>
+			<li>ENTAIREMENT</li>
+			<li>ENTAIREMENT</li>
+			<li>ENTAIREMENT</li>
+			</div>
+		</div>
+		</div>
+
+<div class="topbarbottom">
+<div class="container">	
+	
+</div>
+</div>
+</div>
+<div id="homepage">
+<div id="slider">
+	<div class="sliderleft">
+		<div class="sliderleftbutton">
+			<i class="zmdi zmdi-chevron-left"></i>
+		</div>
+	</div>
+	<div class="sliderarticle">		
+		<div class="slidercontent">
+			<h2>3012 Mint Yeşili Büyülü Abiye</h2>
+		</div>
+	</div>
+	<div class="sliderright">
+		<div class="sliderrightbutton">
+			<i class="zmdi zmdi-chevron-right"></i>
+		</div>
+	</div>
+</div>
+<div class="container">
+<div class="homesidebar">
+	<div class="homesidebarfeatured">
+		<div class="featuredtitle">FEATURED</div><div class=""></div>
+		
+	</div>
+</div>
+<div class="homearticle">
+	<div class="homearticletitle"><h2>YENİ ÜRÜNLER</h2></div>
+</div>
+</div>
+
+</div>
+
+
+
+</html>
